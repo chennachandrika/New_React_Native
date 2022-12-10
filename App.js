5cd0ee77ec4ca1a32fd5bebe3f9fb3864d2d3ec7
Chenna Chandrika <chennachandrikapadmashali@gmail.com>
2022-12-10 13:56:12 +0530

    provider store used

--- App.js
+++ App.js
@@ -4,23 +4,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TodoApp from "./src/components/TodoApp";
 import Todo from "./src/components/Todo";
 
+import {Provider} from "react-redux"
+import {Store} from "./src/redux/store"
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={TodoApp}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen
-          name="Todo"
-          component={Todo}
-          options={{ title: "Edit Todo" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={Store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={TodoApp}
+            options={{ title: "Welcome" }}
+          />
+          <Stack.Screen
+            name="Todo"
+            component={Todo}
+            options={{ title: "Edit Todo" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
