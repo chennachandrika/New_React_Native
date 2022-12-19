243c914cdbe03725725cec7bc9f2986ba441a089
Chenna Chandrika <chennachandrikapadmashali@gmail.com>
2022-12-19 16:28:27 +0530

    redux-saga middleware implemented

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import {createStore,combineReducers,applyMiddleware} from "redux"
+import {createStore,combineReducers,applyMiddleware,compose} from "redux"
 import thunk from "redux-thunk"
+import createSagaMiddleware from 'redux-saga'
+import mySaga from './sagas'
+
 import todoReducer from "./reducers"
+
+let composeEnhancers = compose;
+const sagaMiddleware = createSagaMiddleware()
 
 const rootReducers=combineReducers({todoReducer})
 
-export const Store=createStore(rootReducers,applyMiddleware(thunk))
+export const Store=createStore(rootReducers,composeEnhancers(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(mySaga)
